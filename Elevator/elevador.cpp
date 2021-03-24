3bf627371ce568dfed6ea3cc8f734bc1469d5b96
Isabelle-Matos <isabelle.matos@ufv.br>
2021-03-24 14:06:24 -0300

    Elevator capacity
    First, enter the number of readings taken by the sensor and the capacity of the elevator. Inside the loop, enter the number of people that entering the elevator and left it. From there, the output generates "Y" if the capacity has been exceeded and "N" otherwise.

---
+++ Elevator/elevador.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+   int N; // number of readings (1<=N && N<=1000)
+   int C; // capacity (1<=C && C<=1000)
+
+   int n_exits; 
+   int n_entries;
+   int total_people = 0, totalentries = 0, totalexits = 0;
+
+   bool over = false; 
+
+   int i= 0; //total readings
+   cin >> N >> C;
+
+   while(i<N)
+   {
+       cin >> n_exits;
+       cin >> n_entries;
+       totalentries += n_entries;
+       totalexits += n_exits;
+       total_people = totalentries - totalexits;
+
+       if(total_people >C)
+       {
+           over = true;
+       }
+       i++;
+   }
+   if(over==true){cout<< "Y" << endl;}
+   else{cout <<"N"<< endl;}
+
+    return 0;
+}
